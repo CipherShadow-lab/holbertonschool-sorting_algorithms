eb22146229b43889d7b0a7e40edcd4c4e3ffa40d
CipherShadow <ciphershadow1@gmail.com>
2025-08-08 06:26:05 +1000

    add selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+#include <stddef.h>
+
+/**
+ * selection_sort - function sorts list using Selection  Sort
+ * @array: pointer to array
+ * @size: size of array
+ *
+ * Return: void - nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	size_t min_index;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[  j] < array[min_index])
+			{
+				min_index = j;
+			}
+		}
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
+	}
+}
+
